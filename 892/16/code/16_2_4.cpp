b79fa4ba97d459605fc5e12dd56b854169de0b36
lhxone <lhxone@outlook.com>
2022-08-02 11:35:06 +0800

    16_2_4代码

--- 892/16/code/16_2_4.cpp
+++ 892/16/code/16_2_4.cpp
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <cmath>
-#include <algorithm>
+
+//自定义交换
+void swap(int *x, int *y)
+{
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+//自定义快速排序
+void quick_sort_recursive(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+    int mid = arr[end];
+    int left = start, right = end - 1;
+    while (left < right)
+    {
+        while (arr[left] < mid && left < right)
+            left++;
+        while (arr[right] >= mid && left < right)
+            right--;
+        swap(&arr[left], &arr[right]);
+    }
+    if (arr[left] >= arr[end])
+        swap(&arr[left], &arr[end]);
+    else
+        left++;
+    if (left)
+        quick_sort_recursive(arr, start, left - 1);
+    quick_sort_recursive(arr, left + 1, end);
+}
+
+void quick_sort(int arr[], int len)
+{
+    quick_sort_recursive(arr, 0, len - 1);
+}
 
 int main()
 {
@@ -23,20 +59,10 @@ int main()
         }
         printf("\n");
     }
+    //按行进行排序
     for (int i = 0; i < 4; i++)
     {
-        for (int j = 0; j < 7 - 1; j++)
-        {
-            for (int k = k; k < 7 - 1 - j; k++)
-            {
-                if (a[i][k]< a[i][k + 1])
-                {
-                    int temp = a[i][k];
-                    a[i][k] = a[i][k + 1];
-                    a[i][k + 1] = temp;
-                }
-            }
-        }
+        quick_sort_recursive(a[i], 0, 6);
     }
     //按序打印修改后的数组
     printf("The origin matrix is :\n");
